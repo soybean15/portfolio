cf04b29dfee1857dbe6d7e85543e19e039fd03b8
soybean15 <marlonpadilla1593@gmail.com>
2023-11-23 15:01:08 +0800

    adding hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-    
+  <div class="flex justify-center items-center">
+    <div class="diff  aspect-[16/9] w-[600px]" >
+      <div class="diff-item-1">
+        <div class="flex h-[400px] items-center justify-center">
+          <div class="text-7xl md:text-8xl font-righteous rounded-3xl">
+            <span class="text-orange-800">Soybean</span>
+            <span class="text-green-500">15</span>
+          </div>
+        </div>
+      </div>
+      <div class="diff-item-2">
+        <div class="flex h-[400px] items-center justify-center">
+          <div class="text-7xl md:text-8xl font-righteous bg-orange-700 p-8 rounded-3xl">
+            <span class="">Soybean</span>
+            <span class="text-green-500">15</span>
+          </div>
+        </div>
+      </div>
+      <div class="diff-resizer"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home h-screen">
+
+    <Hero/>
  
   </div>
 </template>
 
 <script>
+import Hero from '@/components/Hero.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    
+    Hero
   }
 }
 </script>
