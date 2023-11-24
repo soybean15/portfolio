9bd1ae5fb5a4e4fb01966d562dd2dcaef2023b71
soybean15 <marlonpadilla1593@gmail.com>
2023-11-24 17:04:23 +0800

    modified hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,37 +3,35 @@
     ref="target"
     class="flex flex-col md:flex justify-around items-start py-10 px-3 md:w-[1000px]"
   >
-    
-      <div class="flex flex-col sm:flex-row items-center" >
-        <div>
-          <img class="w-[1000px]" src="@/assets/logo/lima-bean.png" />
-        </div>
+    <div class="flex flex-col sm:flex-row items-center">
+      <div>
+        <img class="w-[1000px]" src="@/assets/logo/lima-bean.png" />
+      </div>
 
-        <div class="px-5 md:px-10">
-          <div class="text-3xl md:text-5xl font-bold text-start text-green-500">
-            Hi I'm Soybean
-          </div>
-          <p class="text-start text-md md:text-xl py-2">
-            Aspiring web developer and student,I'm diving deep into the world of
-            Vue.js and Laravel. I am passionate about creating dynamic web
-            applications, and I'm enthusiastic about learning and staying
-            updated on industry trends. I'm willing to learn and explore new
-            concepts in web development.
-          </p>
-          <div class="flex py-2">
-            <div v-for="logo in logos" :key="logo">
-              <logo :logo="logo" />
-            </div>
+      <div class="px-5 md:px-10">
+        <div class="text-3xl md:text-5xl font-bold text-start text-green-500">
+          Hi I'm Soybean
+        </div>
+        <p class="text-start text-md md:text-xl py-2">
+          Hi there! I'm Marlon Padilla, a passionate web development enthusiast
+          on a journey to build exciting and user-friendly web applications. I'm
+          diving deep into the world of Vue.js and Laravel, and I'm passionate
+          about creating dynamic web applications. I'm enthusiastic about
+          learning and staying updated on industry trends, and I'm willing to
+          learn and explore new concepts in web development.
+        </p>
+        <div class="flex py-2">
+          <div v-for="logo in logos" :key="logo">
+            <logo :logo="logo" />
           </div>
         </div>
       </div>
-    
+    </div>
   </div>
 </template>
 
 <script>
-
-import { ref,   } from "vue";
+import { ref } from "vue";
 
 import Logo from "./Logo.vue";
 
@@ -61,12 +59,10 @@ const logos = [
 export default {
   components: { Logo },
   setup() {
-
-    return{logos}
+    return { logos };
   },
 };
 </script>
 
 <style>
-
 </style>
