17d701bef3074b664fa0955dada41dd641b9f5bf
soybean15 <marlonpadilla1593@gmail.com>
2023-12-06 23:06:07 +0800

    test meta

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
     {
       name: 'description',
       content: 'My Portfolio',
-      name: 'og:image',
+      property: 'image',
       content: require('@/assets/soybean.png')
     },
   ],
