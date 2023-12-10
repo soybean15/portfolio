52f3db6b64ed43d847f917e21cc09b7fa3db3023
soybean15 <marlonpadilla1593@gmail.com>
2023-12-10 13:11:56 +0800

    updated breakpoint

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col mt-10   px-3 md:w-[1000px] lg:w-[1500px]">
+  <div class="flex flex-col mt-10   px-3 w-full">
     <div class="px-5 md:px-10">
       <div class="text-2xl py-5 md:text-4xl font-bold text-start text-green-500">
         Projects
@@ -16,7 +16,7 @@
         />
          </div>
 
-      <div class="grid  grid-col-1 lg:grid-cols-2 gap-2 py-2">
+      <div class="grid  grid-col-1 lg:grid-cols-2  2xl:grid-cols-3 gap-2 py-2">
         <div v-for="project in projects" :key="project.title">
           <project :project="project" />
         </div>
